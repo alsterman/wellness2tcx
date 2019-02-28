052ba83a97e3cbc87e4141affbf2a7227237cba7
Marcus <marals@kth.se>
2019-02-28 20:18:34 +0100

    Further work. Now know the problem with speed.

--- src/mywellness_extractor/core.clj
+++ src/mywellness_extractor/core.clj
@@ -15,12 +15,12 @@
 
 
 
-(let [raw-data (-> (json/read-str (slurp "input.json"))
+(let [raw-data (-> (json/read-str (slurp "input2.json"))
                    (get "data"))
 
       date-yyyy-MM-dd (get raw-data "date")
 
-      time-hh:mm:ss "16:47:33"
+      time-hh:mm:ss "16:52:38"
 
       starttime-ms (to-unix-time date-yyyy-MM-dd time-hh:mm:ss)
 
@@ -49,8 +49,12 @@
                                        (timestamp))
                            :power (get-in one-sample ["vs" 0])
                            :rpm   (get-in one-sample ["vs" 1])
-                           :speed (get-in one-sample ["vs" 2])}
+                           :speed (get-in one-sample ["vs" 2])
+                           :distance (get-in one-sample ["vs" 3])}
                           )))
+
+      max-speed-kph (reduce max (map (fn [x] (get x :speed)) samples))
+
       tcx-map {
                          :tag     :TrainingCenterDatabase
                          :attrs   {"xsi:schemaLocation" "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2  http://www.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd"
@@ -68,9 +72,9 @@
                                                          {:tag :Id :content [(timestamp starttime-ms)]}
                                                          {:tag     :Lap :attrs {:StartTime (timestamp starttime-ms)}
                                                           :content [
-                                                                    {:tag :TotalTimeSeconds :content [(str total-time-seconds)]}
-                                                                    {:tag :DistanceMeters :content [(str distance-meters)]}
-                                                                    {:tag :MaximumSpeed :content [(str (reduce max (map (fn [x] (get x :speed)) samples)))]}
+                                                                    {:tag :TotalTimeSeconds :content [(format "%.0f" total-time-seconds)]}
+                                                                    ;{:tag :DistanceMeters :content [(str distance-meters)]}
+                                                                    ;{:tag :MaximumSpeed :content [(str (* max-speed-kph 1000))]}
                                                                     {:tag :Intensity    :content ["Active"]}
                                                                     {:tag :TriggerMethod :content ["Manual"]}
                                                                     {:tag     :Track
@@ -80,12 +84,13 @@
                                                                                           {:tag     :Trackpoint
                                                                                            :content [
                                                                                                      {:tag :Time :content [(get sample-point :time)]}
-                                                                                                     {:tag :Cadence :content [(str (get sample-point :rpm))]}
+                                                                                                     ;{:tag :DistanceMeters :content [(format "%.1f" (get sample-point :distance))]}
+                                                                                                     {:tag :Cadence :content [(format "%.0f" (get sample-point :rpm))]}
                                                                                                      {:tag     :Extensions
                                                                                                       :content [{:tag     :TPX
                                                                                                                  :attrs   {"xmlns" "http://www.garmin.com/xmlschemas/ActivityExtension/v2"}
                                                                                                                  :content [
-                                                                                                                           {:tag :Speed :content [(str (/ (get sample-point :speed) 3.6))]}
+                                                                                                                           ;{:tag :Speed :content [(format " %.1f" (/ (get sample-point :speed) 3.6))]}
                                                                                                                            {:tag :Watts :content [(str (int (get sample-point :power)))]}
                                                                                                                            ]}]}
 
@@ -93,7 +98,7 @@
 
                                                                     ]}]}]}]}]
   ;samples
-
+  ;raw-data
   ;dateandtimestr
   ;date-yyyy-MM-dd
   ;(spit (str "json2tcx-" (timestamp starttime-ms) ".tcx") tcx-str)
@@ -104,4 +109,7 @@
   (as-> (with-out-str (xml/emit tcx-map)) $
        (clojure.string/replace  $ #"'" "\"")
        (spit (str "json2tcx-" (timestamp starttime-ms) ".tcx") $))
+
+
+  ;(map (fn [x] (get x :distance)) samples)
   )
